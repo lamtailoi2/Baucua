3c5a0efb00e9ac5daeecf2f5bf596aaaa7eb5fea
Lamtailoi2 <lamtailoi11141@gmail.com>
2024-02-03 02:30:50 +0700

    updated

--- src/JS/main.js
+++ src/JS/main.js
@@ -1,12 +1,47 @@
 const arr = document.querySelectorAll('.cua')
 
-const item = ["huou", "holo", "cua", "ga", "ca", "tom"]
+const items = [
+    {
+        id: 0,
+        name: 'huou'
+    },
+    {
+        id: 1,
+        name: 'holo'
+    },
+    {
+        id: 2,
+        name: 'ga'
+    },
+    {
+        id: 3,
+        name: 'ca'
+    },
+    {
+        id: 4,
+        name: 'cua'
+    },
+    {
+        id: 5,
+        name: 'tom'
+    },
+    
+]
+
+let counts = {
+    cnt1: 0,
+    cnt2: 0,
+    cnt3: 0,
+    cnt4: 0,
+    cnt5: 0,
+    cnt6: 0,
+};
 
 function onclickHandler() {
     let result = []
-    for (let i =0; i < 3; i++) {
-        const randomIndex = Math.floor(Math.random() * 6)
-        result.push(item[randomIndex])
+    for (let i = 0; i < 3; i++) {
+        const randomIndex = Math.floor(Math.random() * 5)
+        result.push(items[randomIndex].name)
     }
     displayResult(result)
 }
@@ -15,6 +50,17 @@ function displayResult(result) {
     console.log(result.toString());
 }
 
-function playerSelection() {
-    let selector = 
+
+
+function increaseCount(spanId) {
+    counts[spanId]++;
+    document.getElementById(spanId).innerHTML = counts[spanId];
+}
+
+function playerSelection(animal) {
+    let counts = [];
+    increaseCount();
+    let selection = document.querySelector(`.${animal}`)  
+    
+
 }
